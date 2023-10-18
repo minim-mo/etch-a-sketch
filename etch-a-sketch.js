415cd1697f5117efaca8b722ca8e2c6f461201fe
minim-mo <147269904+minim-mo@users.noreply.github.com>
2023-10-18 15:24:59 -0400

    addEventListener

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,9 +3,15 @@ console.log("`Let's build an awesome project!!!`");
 // add 64x64 in grid
 
 document.addEventListener("DOMContentLoaded", function() {
-    createBoard(16);
-    getSize()
+    createBoard(16); //default set at 16
     console.log("hello!")
+
+    let btn_popup = document.querySelector("#popup");
+    //if user click the button that will tricker the function that the size of the board will call the function getSize();
+    btn_popup.addEventListener("click" , function(){
+        let size = getSize();
+        createBoard(size);
+    });
     
 });
 
@@ -32,9 +38,23 @@ function createBoard(size) {
      
 }   
 
-//add user input using window.prompt();
+//add user userChoice using window.prompt();
 // set min 0 max 100
 // so we need to create size choosing function
 function getSize() {
     let userChoice = prompt("Enter your size of choice to start the game board!");
+
+    //check if userChoice == "" asked again!
+    let message = document.querySelector("#message");
+    if(userChoice == "") {
+        message.innerHTML = "You didn't enter a number!";
+    }
+    else if (userChoice <= 0 || userChoice > 100 ) {
+        message.innerHTML = "Enter a number between 1-100"
+    }
+    else {
+        message.innerHTML = "Let's play this game!"
+        return userChoice;
+    }
+    console.log(message);
 }

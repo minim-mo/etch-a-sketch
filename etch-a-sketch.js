d01bd4a750ff1787907fefd6b64c1bab8785fd9d
minim-mo <147269904+minim-mo@users.noreply.github.com>
2023-10-18 14:33:03 -0400

    create grid template function and test default 16x16grid in js

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,19 @@
 console.log("`Let's build an awesome project!!!`");
 
-//create div
-const container = document.querySelector('#grid-container');
-
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the content!';
-
-container.appendChild(content);
-
 //make click function 
-// add 64x64 grid
+// add 64x64 in grid
+
+document.addEventListener("DOMContentLoaded", function() {
+    createBoard(16);
+    console.log("hello!")
+});
+
+//create function called createBoard()
+function createBoard(size) {
+    let board = document.querySelector(".board");
+
+    //turn 16x16 grid size into variable that user could change later on.
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+}

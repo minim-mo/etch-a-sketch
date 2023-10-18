cd462d52864156bb6ad7a186df5aa01fc6ef604f
minim-mo <147269904+minim-mo@users.noreply.github.com>
2023-10-18 14:10:20 -0400

    styling the page html, css

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,13 @@
 console.log("`Let's build an awesome project!!!`");
+
+//create div
+const container = document.querySelector('#grid-container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the content!';
+
+container.appendChild(content);
+
+//make click function 
+// add 64x64 grid

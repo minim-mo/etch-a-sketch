84c2ba89a4d762573f175bea11361ca7aa9350a9
minim-mo <147269904+minim-mo@users.noreply.github.com>
2023-10-18 14:55:38 -0400

    added getSize function for user input

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,12 @@
 console.log("`Let's build an awesome project!!!`");
 
-//make click function 
 // add 64x64 in grid
 
 document.addEventListener("DOMContentLoaded", function() {
     createBoard(16);
+    getSize()
     console.log("hello!")
+    
 });
 
 //create function called createBoard()
@@ -22,10 +23,18 @@ function createBoard(size) {
     // div in columns and rows.
     let numDiv = size * size;
 
-    for (let i = 0; i < numDiv; i++);
-    // to insert element using insertAdjacentElement()
+    for (let i = 0; i < numDiv; i++); {
+        // to insert element using insertAdjacentElement()
         let div = document.createElement("div");
         div.style.backgroundColor = "gray"; // add color to the div
         board.insertAdjacentElement("beforeend", div);
+    }
+     
+}   
 
+//add user input using window.prompt();
+// set min 0 max 100
+// so we need to create size choosing function
+function getSize() {
+    let userChoice = prompt("Enter your size of choice to start the game board!");
 }
